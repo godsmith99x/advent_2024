2ba784ae64a300772003900a3d365e21523a830a
Joel Godfrey-Smith <joel.godfrey-smith@us.labn.net>
2024-12-17 10:42:16 -0500

    Clarified daily folder structure

--- day1/tests/pt1.py
+++ day1/tests/pt1.py
@@ -5,11 +5,11 @@ import os
 # Add the parent directory to the sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-from day1.day1pt2 import similarity_score 
+from day1.pt1 import total_distance 
 
-class TestSimilarityScore(unittest.TestCase):
-    def test_similarity_score(self):
-        self.assertEqual(similarity_score("day1/tests/test_input.csv"), 31)
+class TestTotalDistance(unittest.TestCase):
+    def test_total_distance(self):
+        self.assertEqual(total_distance("day1/tests/test_input.csv"), 11)
 
 if __name__ == "__main__":
     unittest.main()
